df0b1253ee858ccdf849fe0af6fe94b6e9b668a6
biswas98 <biswasbiplab98@gmail.com>
2023-06-30 23:53:32 +0530

    added 1970 question

--- Leetcode/Questions/faltu.cpp
+++ Leetcode/Questions/faltu.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
-    cout<<"Hello World"<<endl;
+    int i = 0;
+    vector<vector<int>> myVec = {{1,2,3},{4,5,6},{7,8,9}};
+
+    // vector<int>::iterator it = myVec[].begin();
+
+    
+    while(i < myVec.size()){
+        vector<int>::iterator it = myVec[i].begin();
+        while (it != myVec[i].end())
+        {   
+            cout<<*it<<" ";
+            it++;
+        }
+        cout<<endl;
+        i++;        
+    }
+  
+
+    // cout<<myVec.size();
+
     return 0;
 }
