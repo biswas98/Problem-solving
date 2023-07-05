5cabfee0b2ae901fd25773506748eb23f1ca6986
biswas98 <biswasbiplab98@gmail.com>
2023-07-05 23:51:53 +0530

    advancement in question 15

--- Leetcode/Questions/15.cpp
+++ Leetcode/Questions/15.cpp
@@ -6,6 +6,29 @@ using namespace std;
 int main()
 {
 
-    
+    vector<int> test_Data = {-1,0,1,2,-1,-4};
+
+    vector<int>::iterator it = test_Data.begin();
+    vector<int>::iterator it_next = test_Data.begin()+1;
+    vector<int>::iterator it_next_next = test_Data.begin()+2;
+
+    while(it != test_Data.end()){
+
+        while(it_next != test_Data.end()){
+
+            while( it_next_next != test_Data.end()){
+
+                if( (*it + *it_next + *it_next_next) == 0 ){
+
+                    cout<<"[ "<<*it<<", "<<*it_next<<", "<<*it_next_next<<" ]"<<endl;
+                }
+
+                it_next_next++;
+            }
+            it_next++;
+        }
+        it++;
+    }
+
     return 0;
 }
