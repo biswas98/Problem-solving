127b3b328e05125bca99752916932eda9702f3e9
biswas98 <biswasbiplab98@gmail.com>
2023-07-05 21:45:46 +0530

    learning more about question 15

--- Leetcode/Questions/15.cpp
+++ Leetcode/Questions/15.cpp
@@ -3,60 +3,58 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     vector<vector<int>> container_Vec = {};
-    
+
     vector<int> triplets = {};
-    vector<int> triplets_sec = {};
 
-    triplets.push_back(8);
-    triplets.push_back(2);
-    triplets.push_back(3);
-    
-    triplets_sec.push_back(10);
-    triplets_sec.push_back(11);
-    triplets_sec.push_back(12);
+    int num_of_array, container_index = 0;
 
+    cout << "How many 1D arrays: ";
+    cin >> num_of_array;
 
-    vector<int>::iterator it = triplets.begin();
-    vector<int>::iterator it_sec = triplets_sec.begin();
-
-    while (it != triplets.end())
+    while (container_index < num_of_array)
     {
-        cout<<*it<<" ";
-        it++;
-    }
-    cout<<endl;
-   
-    while (it_sec != triplets_sec.end())
-    {
-        cout<<*it_sec<<" ";
-        it_sec++;
-    }
-    cout<<endl;
-   
-    
+        int size, el = 0;
+        triplets = {};
 
-    // cout<<container_Vec.size()<<endl;
-    // container_Vec.push_back(triplets);
-    // container_Vec.push_back(triplets_sec);
-    // cout<<container_Vec.size()<<endl;
+        cout << "how many element in vector: ";
+        cin >> size;
 
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Enter element: ";
+            cin >> el;
+            triplets.push_back(el);
+        }
+
+        vector<int>::iterator it = triplets.begin();
+        while (it != triplets.end())
+        {
+            cout << *it << " ";
+            it++;
+        }
+        cout << endl;
+        container_Vec.push_back(triplets);      
+        container_index++;
+    }
 
     // Need to work on this logic
     int i = 0;
-    while(i < container_Vec.size()){
+    cout<<"2d Vector is: "<<endl;
+    while (i < container_Vec.size())
+    {
         vector<int>::iterator it_c = container_Vec[i].begin();
         while (it_c != container_Vec[i].end())
-        {   
-            cout<<*it_c<<" ";
+        {
+            cout << *it_c << " ";
             it_c++;
         }
-        cout<<endl;
-        i++;        
+        cout << endl;
+        i++;
     }
-    
 
     return 0;
 }
