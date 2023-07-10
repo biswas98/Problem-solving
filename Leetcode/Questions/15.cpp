b2ed72a6c6bf3611807835a132782966d7ed6788
biswas98 <biswasbiplab98@gmail.com>
2023-07-10 20:54:38 +0530

    minor changes

--- Leetcode/Questions/15.cpp
+++ Leetcode/Questions/15.cpp
@@ -7,108 +7,97 @@ using namespace std;
 class Solution
 {
 public:
-    vecter<vector<int>> threeSum(vector<int> &nums)
+    vector<vector<int>> threeSum(vector<int> &nums)
     {
+
+        vector<vector<int>> container = {};
+        vector<int> temp = {};
+        vector<int> temp_compare = {};
+
+        vector<int>::iterator it = nums.begin();
+        vector<int>::iterator it_next = nums.begin() + 1;
+        vector<int>::iterator it_next_next = nums.begin() + 2;
+
+        for (it = nums.begin(); it != nums.end(); it++)
+        {
+
+            if (
+                it != it_next &&
+                it_next != it_next_next &&
+                it_next_next != it)
+            {
+
+                for (it_next = nums.begin() + 1; it_next != nums.end(); it_next++)
+                {
+                    if (
+                        it != it_next &&
+                        it_next != it_next_next &&
+                        it_next_next != it)
+                    {
+
+                        for (it_next_next = nums.begin() + 2; it_next_next != nums.end(); it_next_next++)
+                        {
+
+                            if (
+                                (*it + *it_next + *it_next_next) == 0 &&
+                                it != it_next &&
+                                it_next != it_next_next &&
+                                it_next_next != it)
+
+                            {
+                                // cout << "[ " << *it << ", " << *it_next << ", " << *it_next_next << " ]" << endl;
+                                temp = {};
+                                temp.push_back(*it);
+                                temp.push_back(*it_next);
+                                temp.push_back(*it_next_next);
+
+                                int i = 0, present_check = 0;
+                                while (i != container.size())
+                                {
+                                    sort(container[i].begin(), container[i].end());
+                                    if (container[i] == temp)
+                                        present_check = 1;
+                                    i++;
+                                }
+                                if (present_check == 0)
+                                    container.push_back(temp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+        return container;
+
     }
 };
 
 int main()
 {
     Solution obj;
-    
-    vector<int> test_Data = {-1, 0, 1, 2, -1, -4};
 
-    vector<vector<int>> container = {};
-    vector<int> temp = {};
-    vector<int> temp_compare = {};
+    vector<int> nums = {0,0,0};
 
+    vector<vector<int>> ret = {};
 
-    vector<int>::iterator it = test_Data.begin();
-    vector<int>::iterator it_next = test_Data.begin() + 1;
-    vector<int>::iterator it_next_next = test_Data.begin() + 2;
+    ret = obj.threeSum(nums);
 
-    // while(it != test_Data.end()){
-
-    //     while(it_next != test_Data.end()){
-
-    //         while( it_next_next != test_Data.end()){
-
-    //             if( (*it + *it_next + *it_next_next) == 0 ){
-
-    //                 cout<<"[ "<<*it<<", "<<*it_next<<", "<<*it_next_next<<" ]"<<endl;
-    //             }
-
-    //             it_next_next++;
-    //         }
-    //         it_next++;
-    //     }
-    //     it++;
-    // }
-
-    for (it = test_Data.begin(); it != test_Data.end(); it++)
-    {
-
-        if (
-            it != it_next &&
-            it_next != it_next_next &&
-            it_next_next != it)
-        {
-
-            for (it_next = test_Data.begin() + 1; it_next != test_Data.end(); it_next++)
-            {
-                if (
-                    it != it_next &&
-                    it_next != it_next_next &&
-                    it_next_next != it)
-                {
-
-                    for (it_next_next = test_Data.begin() + 2; it_next_next != test_Data.end(); it_next_next++)
-                    {
-
-                        if (
-                            (*it + *it_next + *it_next_next) == 0 &&
-                            it != it_next &&
-                            it_next != it_next_next &&
-                            it_next_next != it)
-
-                        {
-                            // cout << "[ " << *it << ", " << *it_next << ", " << *it_next_next << " ]" << endl;
-                            temp = {};
-                            temp.push_back(*it);
-                            temp.push_back(*it_next);
-                            temp.push_back(*it_next_next);
-
-                            int i = 0, present_check = 0;
-                            while (i != container.size())
-                            {
-                                sort(container[i].begin(), container[i].end());
-                                if (container[i] == temp)
-                                    present_check = 1;
-                                i++;
-                            }
-                            if (present_check == 0)
-                                container.push_back(temp);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    cout << container.size() << endl;
+    // cout << container.size() << endl;
 
     int i = 0;
-    while (i < container.size())
+    while (i < ret.size())
     {
 
-        vector<int>::iterator it_container = container[i].begin();
-        while (it_container != container[i].end())
+        vector<int>::iterator it_ret = ret[i].begin();
+        while (it_ret != ret[i].end())
         {
-            cout << *it_container << " ";
-            it_container++;
+            cout << *it_ret << " ";
+            it_ret++;
         }
         cout << endl;
         i++;
     }
+    
     return 0;
 }
