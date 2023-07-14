0af545bb889eaf03ca693c8e660eab10cbfc8b41
biswas98 <biswasbiplab98@gmail.com>
2023-07-14 23:21:11 +0530

    minor changes

--- Leetcode/Learning/set_learning.cpp
+++ Leetcode/Learning/set_learning.cpp
@@ -5,30 +5,27 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     // 1-D set
-    set<int> mySet = {1,1,1};
+    set<int> mySet = {1, 1, 1};
 
     set<int>::iterator it = mySet.begin();
 
-    while(it != mySet.end()){
-        cout<<*it<<" ";
+    while (it != mySet.end())
+    {
+        cout << *it << " ";
         it++;
     }
-    cout<<endl;
-    cout<<"Size of container: "<<mySet.size();
+    cout << endl;
+    cout << "Size of 1D-Set: " << mySet.size() << endl;
 
-    //2-D set 
+ 
 
-    set<set<int>> my2Dset = {{4,3,2,1},{1,2,3,4},{4,3,2,1}};
+    set<set<int>> my2Dset = {{4, 3, 2, 1}, {1, 2, 3, 4}, {4, 3, 2, 1}, {2, 6, 1, 2}};
 
-    int i = 0;
-    while(i != my2Dset.size()){
-        set<int>::iterator it_2D = my2Dset[i].begin();
-
-        
-    }
+   
     
 
     return 0;
