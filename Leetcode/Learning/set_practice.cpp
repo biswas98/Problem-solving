c2585195b88995e26d9c260339557ee561b664a4
biswas98 <biswasbiplab98@gmail.com>
2023-07-21 21:31:01 +0530

    added comments for help

--- Leetcode/Learning/set_practice.cpp
+++ Leetcode/Learning/set_practice.cpp
@@ -8,8 +8,10 @@ using namespace std;
 int main(){
     // comparing "vector" with "set"
 
+    // Vector implementation 
     vector<int> v1 = {2,2,3,3,0,0,1,1};
 
+    // need to sort the vector
     sort(v1.begin(),v1.end());
 
     vector<int>::iterator itv = v1.begin();
@@ -20,6 +22,7 @@ int main(){
     }
     cout<<endl;
 
+    // "set" implementation - set stores only unique element and it doesn't need to be sorted, all the elements are already sorted
     set<int> s1 = {2,2,3,3,0,0,1,1};
 
     set<int>::iterator its = s1.begin();
