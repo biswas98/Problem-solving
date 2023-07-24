12b56824ff0a7bb6d9119186e51c371ccf0f6786
biswas98 <biswasbiplab98@gmail.com>
2023-07-24 22:15:10 +0530

    advancement in learing iterator

--- Leetcode/Learning/auto_iterator.cpp
+++ Leetcode/Learning/auto_iterator.cpp
@@ -8,12 +8,24 @@ using namespace std;
 
 int main(){
 
+    
     vector<int> v1 = {1,1,2,4};
 
     for(auto i : v1){
         cout<<i<<" ";
     }
     cout<<endl;
+
+
+    vector<vector<int>> v2 = {{1,0,1},{0,1,0},{0,0,0}};
+
+    for( auto r : v2){
+        for( auto c: r){
+            cout<<c<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 
 }
