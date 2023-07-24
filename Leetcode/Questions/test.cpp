22eef07577eb2c658a49d6a7ea4d71aadc4b88c4
biswas98 <biswasbiplab98@gmail.com>
2023-07-24 20:08:15 +0530

    learning enhancement

--- Leetcode/Questions/test.cpp
+++ Leetcode/Questions/test.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-    vector<int>  vec1 = {0,1,0,1,1,1,0,0, 0,0, 1};
+    // vector<int>  vec1 = {0,1,0,1,1,1,0,0, 0,0, 1};
 
-    vector<int>::iterator it = vec1.begin();
+    // vector<int>::iterator it = vec1.begin();
 
-    sort(vec1.begin(),vec1.end());
+    // sort(vec1.begin(),vec1.end());
 
-    while (it != vec1.end())
-    {
-        cout<<*it<<" ";
-        it++;
-    }
+    // while (it != vec1.end())
+    // {
+    //     cout<<*it<<" ";
+    //     it++;
+    // }
     
-    cout<<endl;
+    // cout<<endl;
+    
+    set<set<int>> mySet = {{1,2,3},{4,5,6},{7,8,9}};
+
+    cout<<mySet.size();
     
 
     return 0;
